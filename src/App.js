3105ce536b505f5bace36ec8ce902930ad2ed8b9
JeongHoon Lee <sherry0_@kakao.com>
2022-07-09 17:29:53 +0900

    fixed travis.yml

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn sad
+                    Learn React
                 </a>
             </header>
         </div>
